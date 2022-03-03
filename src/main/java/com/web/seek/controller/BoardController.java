93a44dd400cde8d750fb97239960a2c241f2bba6
an-in-seek <dktmrorl@naver.com>
2022-03-03 22:03:39 +0900

    스프링부트 및 타임리프 연동 코드 최초 커밋

--- src/main/java/com/web/seek/controller/BoardController.java
+++ src/main/java/com/web/seek/controller/BoardController.java
@@ -15,12 +15,24 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    /**
+     * 게시글 작성 폼 데이터 조회
+     * @param id
+     * @param model
+     * @return
+     */
     @GetMapping({"", "/"})
     public String board(@RequestParam(value = "id", defaultValue = "0") Long id, Model model) {
         model.addAttribute("board", boardService.findBoardById(id));
         return "/board/form";
     }
 
+    /**
+     * 게시글 리스트 조회
+     * @param pageable
+     * @param model
+     * @return
+     */
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model) {
         model.addAttribute("boardList", boardService.findBoardList(pageable));
---
+++ src/main/java/com/web/seek/controller/BoardRestController.java
@@ -0,0 +1,52 @@
+package com.web.seek.controller;
+
+import com.web.seek.domain.Board;
+import com.web.seek.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/boards")
+@RequiredArgsConstructor
+public class BoardRestController {
+
+    private final BoardService boardService;
+
+    /**
+     *
+     * @param board
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<?> postBoard(@RequestBody Board board) {
+        Board savedBoard = boardService.save(board);
+        return new ResponseEntity<>(savedBoard, HttpStatus.CREATED);
+    }
+
+    /**
+     *
+     * @param id
+     * @param board
+     * @return
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putBoard(@PathVariable("id") Long id, @RequestBody Board board) {
+        Board persistBoard = boardService.findBoardById(id);
+        persistBoard.update(board);
+        Board savedBoard = boardService.save(persistBoard);
+        return new ResponseEntity<>(savedBoard, HttpStatus.OK);
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBoard(@PathVariable("id") Long id) {
+        boardService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
